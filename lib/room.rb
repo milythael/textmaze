bf49dce00271635e619ba91571a46c8eff69e146
Sean Carley <milythael.mourning@gmail.com>
2016-09-14 08:49:35 -0400

    flesh out rooms

--- lib/room.rb
+++ lib/room.rb
@@ -1,5 +1,17 @@
 class Room
+  def initialize(exits = {})
+    @exits = exits
+  end
+
+  def show_exit(direction)
+    @exits[direction]
+  end
+
   def exits
-    []
+    @exits.keys
+  end
+
+  def add_exit(direction, room)
+    @exits[direction] = room
   end
 end
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -2,8 +2,61 @@ require 'room'
 RSpec.describe Room do
   describe "Empty Room with No Doors" do
     let(:doorless) { Room.new }
+
     it "has no exits" do
-      doorless.exits.length == 0
+      expect(doorless.exits.length).to eq 0
+    end
+  end
+
+  describe "Empty Room with No Walls" do
+    let(:wallless) do
+      room = Room.new
+      room.add_exit("north", room)
+      room.add_exit("south", room)
+      room.add_exit("east", room)
+      room.add_exit("west", room)
+    end
+
+    it "has four exits" do
+      expect(wallless.exits.length).to eq 4
+    end
+
+    it "has the right exits" do
+      expect(wallless.exits).to contain_exactly("north", "east", "south", "west")
+    end
+
+    it "should return nothing for a non-existent direction" do
+      expect(wallless.show_exit("in")).to eq nil
+    end
+  end
+
+  describe "Two Connected Rooms" do
+    let(:left) {Room.new}
+    let(:right) {Room.new}
+
+    before do
+      left.add_exit("right", right)
+      right.add_exit("left", left)
+    end
+
+    it "left room should have one exit" do
+      expect(left.exits.length).to eq 1
+    end
+
+    it "right room should have one exit" do
+      expect(right.exits.length).to eq 1
+    end
+
+    it "left should connect to right" do
+      expect(left.exits[0]).to eq "right"
+    end
+
+    it "right should connect to left" do
+      expect(right.exits[0]).to eq "left"
+    end
+
+    it "right room, left exit should lead to left room" do
+      expect(right.show_exit(right.exits[0])).to eq left
     end
   end
 end
